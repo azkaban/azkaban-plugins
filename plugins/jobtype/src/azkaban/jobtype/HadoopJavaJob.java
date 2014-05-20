b8e35bc4e02be1f951d5f1895c30082d7af60366
Anthony Hsu <erwaman@gmail.com>
2014-05-20 11:45:07 -0700

    Fixed indentation (changed tabs to 2 spaces) in the files I modified in my last commit

--- plugins/jobtype/src/azkaban/jobtype/HadoopJavaJob.java
+++ plugins/jobtype/src/azkaban/jobtype/HadoopJavaJob.java
@@ -35,230 +35,224 @@ import static org.apache.hadoop.security.UserGroupInformation.HADOOP_TOKEN_FILE_
 
 public class HadoopJavaJob extends JavaProcessJob {
 
-	public static final String RUN_METHOD_PARAM = "method.run";
-	public static final String CANCEL_METHOD_PARAM = "method.cancel";
-	public static final String PROGRESS_METHOD_PARAM = "method.progress";
+  public static final String RUN_METHOD_PARAM = "method.run";
+  public static final String CANCEL_METHOD_PARAM = "method.cancel";
+  public static final String PROGRESS_METHOD_PARAM = "method.progress";
 
-	public static final String JOB_CLASS = "job.class";
-	public static final String DEFAULT_CANCEL_METHOD = "cancel";
-	public static final String DEFAULT_RUN_METHOD = "run";
-	public static final String DEFAULT_PROGRESS_METHOD = "getProgress";
+  public static final String JOB_CLASS = "job.class";
+  public static final String DEFAULT_CANCEL_METHOD = "cancel";
+  public static final String DEFAULT_RUN_METHOD = "run";
+  public static final String DEFAULT_PROGRESS_METHOD = "getProgress";
 
-	private static final String HADOOP_SECURITY_MANAGER_CLASS_PARAM = "hadoop.security.manager.class";
-	
-	private String _runMethod;
-	private String _cancelMethod;
-	private String _progressMethod;
+  private static final String HADOOP_SECURITY_MANAGER_CLASS_PARAM = "hadoop.security.manager.class";
 
-	private Object _javaObject = null;
-	
-	private String userToProxy = null;
-	private boolean shouldProxy = false;
-	private boolean obtainTokens = false;
-	private boolean noUserClasspath = false;
-	
-	private HadoopSecurityManager hadoopSecurityManager;
+  private String _runMethod;
+  private String _cancelMethod;
+  private String _progressMethod;
 
-	public HadoopJavaJob(String jobid, Props sysProps, Props jobProps, Logger log) throws RuntimeException {
-		super(jobid, sysProps, jobProps, log);
-		
-		getJobProps().put("azkaban.job.id", jobid);
-		shouldProxy = getSysProps().getBoolean("azkaban.should.proxy", false);
-		getJobProps().put("azkaban.should.proxy", Boolean.toString(shouldProxy));
-		obtainTokens = getSysProps().getBoolean("obtain.binary.token", false);
-		noUserClasspath = getSysProps().getBoolean("azkaban.no.user.classpath", false);
-		
-		if(shouldProxy) {
-			getLog().info("Initiating hadoop security manager.");
-			try {
-				hadoopSecurityManager = loadHadoopSecurityManager(sysProps, log);
-			}
-			catch(RuntimeException e) {
-				e.printStackTrace();
-				throw new RuntimeException("Failed to get hadoop security manager!" + e.getCause());
-			}
-		}
-	}
-	
-	private HadoopSecurityManager loadHadoopSecurityManager(Props props, Logger logger) throws RuntimeException {
+  private Object _javaObject = null;
 
-		Class<?> hadoopSecurityManagerClass = props.getClass(HADOOP_SECURITY_MANAGER_CLASS_PARAM, true, HadoopJavaJob.class.getClassLoader());
-		getLog().info("Initializing hadoop security manager " + hadoopSecurityManagerClass.getName());
-		HadoopSecurityManager hadoopSecurityManager = null;
+  private String userToProxy = null;
+  private boolean shouldProxy = false;
+  private boolean obtainTokens = false;
+  private boolean noUserClasspath = false;
 
-		try {
-			Method getInstanceMethod = hadoopSecurityManagerClass.getMethod("getInstance", Props.class);
-			hadoopSecurityManager = (HadoopSecurityManager) getInstanceMethod.invoke(hadoopSecurityManagerClass, props);
-		} 
-		catch (InvocationTargetException e) {
-			getLog().error("Could not instantiate Hadoop Security Manager "+ hadoopSecurityManagerClass.getName() + e.getCause());
-			throw new RuntimeException(e.getCause());
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e.getCause());
-		}
+  private HadoopSecurityManager hadoopSecurityManager;
 
-		return hadoopSecurityManager;
-	}
+  public HadoopJavaJob(String jobid, Props sysProps, Props jobProps, Logger log) throws RuntimeException {
+    super(jobid, sysProps, jobProps, log);
 
-	@Override
-	protected String getJVMArguments() {
-		String args = super.getJVMArguments();
+    getJobProps().put("azkaban.job.id", jobid);
+    shouldProxy = getSysProps().getBoolean("azkaban.should.proxy", false);
+    getJobProps().put("azkaban.should.proxy", Boolean.toString(shouldProxy));
+    obtainTokens = getSysProps().getBoolean("obtain.binary.token", false);
+    noUserClasspath = getSysProps().getBoolean("azkaban.no.user.classpath", false);
 
-		String typeUserGlobalJVMArgs = getJobProps().getString("jobtype.global.jvm.args", null);
-		if (typeUserGlobalJVMArgs != null) {
-			args += " " + typeUserGlobalJVMArgs;
-		}
-		String typeSysGlobalJVMArgs = getSysProps().getString("jobtype.global.jvm.args", null);
-		if (typeSysGlobalJVMArgs != null) {
-			args += " " + typeSysGlobalJVMArgs;
-		}
-		String typeUserJVMArgs = getJobProps().getString("jobtype.jvm.args", null);
-		if (typeUserJVMArgs != null) {
-			args += " " + typeUserJVMArgs;
-		}
-		String typeSysJVMArgs = getSysProps().getString("jobtype.jvm.args", null);
-		if (typeSysJVMArgs != null) {
-			args += " " + typeSysJVMArgs;
-		}
-		return args;
-	}
-	
-	@Override
-	protected List<String> getClassPaths() {
-		List<String> classPath;
-		if(!noUserClasspath) {
-			classPath = super.getClassPaths();
-		}
-		else {
-			getLog().info("Supressing user supplied classpath settings.");
-			classPath = new ArrayList<String>();
-		}
+    if (shouldProxy) {
+      getLog().info("Initiating hadoop security manager.");
+      try {
+        hadoopSecurityManager = loadHadoopSecurityManager(sysProps, log);
+      } catch (RuntimeException e) {
+        e.printStackTrace();
+        throw new RuntimeException("Failed to get hadoop security manager!" + e.getCause());
+      }
+    }
+  }
 
-		classPath.add(getSourcePathFromClass(HadoopJavaJobRunnerMain.class));
-		classPath.add(getSourcePathFromClass(Props.class));
-		classPath.add(getSourcePathFromClass(HadoopSecurityManager.class));
-		
-		List<String> typeClassPath = getSysProps().getStringList("jobtype.classpath", null, ",");
-		if(typeClassPath != null) {
-			// fill in this when load this jobtype
-			String pluginDir = getSysProps().get("plugin.dir");
-			for(String jar : typeClassPath) {
-				File jarFile = new File(jar);
-				if(!jarFile.isAbsolute()) {
-					jarFile = new File(pluginDir + File.separatorChar + jar);
-				}
-				
-				if(!classPath.contains(jarFile.getAbsoluteFile())) {
-					classPath.add(jarFile.getAbsolutePath());
-				}
-			}
-		}
-		
-		
-		List<String> typeGlobalClassPath = getSysProps().getStringList("jobtype.global.classpath", null, ",");
-		if(typeGlobalClassPath != null) {
-			for(String jar : typeGlobalClassPath) {
-				if(!classPath.contains(jar)) {
-					classPath.add(jar);
-				}
-			}
-		}
+  private HadoopSecurityManager loadHadoopSecurityManager(Props props, Logger logger) throws RuntimeException {
 
-		return classPath;
-	}
+    Class<?> hadoopSecurityManagerClass =
+        props.getClass(HADOOP_SECURITY_MANAGER_CLASS_PARAM, true, HadoopJavaJob.class.getClassLoader());
+    getLog().info("Initializing hadoop security manager " + hadoopSecurityManagerClass.getName());
+    HadoopSecurityManager hadoopSecurityManager = null;
 
-	@Override
-	public void run() throws Exception {
-		File f = null;
-		if(shouldProxy && obtainTokens) {
-			userToProxy = getJobProps().getString("user.to.proxy");
-			getLog().info("Need to proxy. Getting tokens.");
-			Props props = new Props();
-			props.putAll(getJobProps());
-			props.putAll(getSysProps());
-			
-			f = getHadoopTokens(props);
-			getJobProps().put("env." + HADOOP_TOKEN_FILE_LOCATION, f.getAbsolutePath());
-		}
-		try {
-			super.run();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new Exception(e);
-		}
-		finally {
-			if(f != null) {
-				try{
-					cancelHadoopTokens(f);
-				} catch (Throwable t) {
-					t.printStackTrace();
-					getLog().error("Failed to cancel tokens.");
-				}
-				if(f.exists()) {
-					f.delete();
-				}
-			}
-		}
-	}
-	
-	private static String getSourcePathFromClass(Class<?> containedClass) {
-		File file = new File(containedClass.getProtectionDomain().getCodeSource().getLocation().getPath());
+    try {
+      Method getInstanceMethod = hadoopSecurityManagerClass.getMethod("getInstance", Props.class);
+      hadoopSecurityManager = (HadoopSecurityManager) getInstanceMethod.invoke(hadoopSecurityManagerClass, props);
+    } catch (InvocationTargetException e) {
+      getLog().error(
+          "Could not instantiate Hadoop Security Manager " + hadoopSecurityManagerClass.getName() + e.getCause());
+      throw new RuntimeException(e.getCause());
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException(e.getCause());
+    }
 
-		if (!file.isDirectory() && file.getName().endsWith(".class")) {
-			String name = containedClass.getName();
-			StringTokenizer tokenizer = new StringTokenizer(name, ".");
-			while(tokenizer.hasMoreTokens()) {
-				tokenizer.nextElement();
+    return hadoopSecurityManager;
+  }
 
-				file = file.getParentFile();
-			}
-			return file.getPath();
-		}
-		else {
-			return containedClass.getProtectionDomain().getCodeSource().getLocation().getPath();
-		}
-	}
-	
-	protected File getHadoopTokens(Props props) throws HadoopSecurityManagerException {
+  @Override
+  protected String getJVMArguments() {
+    String args = super.getJVMArguments();
 
-		File tokenFile = null;
-		try {
-			tokenFile = File.createTempFile("mr-azkaban", ".token");
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new HadoopSecurityManagerException("Failed to create the token file.", e);
-		}
-		
-		hadoopSecurityManager.prefetchToken(tokenFile, props, getLog());
-		
-		return tokenFile;
-	}
-	
-	private void cancelHadoopTokens(File f) {
-		try {
-			hadoopSecurityManager.cancelTokens(f, userToProxy, getLog());
-		} catch (HadoopSecurityManagerException e) {
-			e.printStackTrace();
-			getLog().error(e.getCause() + e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			getLog().error(e.getCause() + e.getMessage());
-		}
-		
-	}
-	
-	@Override
-	protected String getJavaClass() {
-		return HadoopJavaJobRunnerMain.class.getName();
-	}
+    String typeUserGlobalJVMArgs = getJobProps().getString("jobtype.global.jvm.args", null);
+    if (typeUserGlobalJVMArgs != null) {
+      args += " " + typeUserGlobalJVMArgs;
+    }
+    String typeSysGlobalJVMArgs = getSysProps().getString("jobtype.global.jvm.args", null);
+    if (typeSysGlobalJVMArgs != null) {
+      args += " " + typeSysGlobalJVMArgs;
+    }
+    String typeUserJVMArgs = getJobProps().getString("jobtype.jvm.args", null);
+    if (typeUserJVMArgs != null) {
+      args += " " + typeUserJVMArgs;
+    }
+    String typeSysJVMArgs = getSysProps().getString("jobtype.jvm.args", null);
+    if (typeSysJVMArgs != null) {
+      args += " " + typeSysJVMArgs;
+    }
+    return args;
+  }
 
-	@Override
-	public String toString() {
-		return "JavaJob{" + "_runMethod='" + _runMethod + '\''
-				+ ", _cancelMethod='" + _cancelMethod + '\''
-				+ ", _progressMethod='" + _progressMethod + '\''
-				+ ", _javaObject=" + _javaObject + ", props="
-				+ getJobProps() + '}';
-	}
+  @Override
+  protected List<String> getClassPaths() {
+    List<String> classPath;
+    if (!noUserClasspath) {
+      classPath = super.getClassPaths();
+    } else {
+      getLog().info("Supressing user supplied classpath settings.");
+      classPath = new ArrayList<String>();
+    }
+
+    classPath.add(getSourcePathFromClass(HadoopJavaJobRunnerMain.class));
+    classPath.add(getSourcePathFromClass(Props.class));
+    classPath.add(getSourcePathFromClass(HadoopSecurityManager.class));
+
+    List<String> typeClassPath = getSysProps().getStringList("jobtype.classpath", null, ",");
+    if (typeClassPath != null) {
+      // fill in this when load this jobtype
+      String pluginDir = getSysProps().get("plugin.dir");
+      for (String jar : typeClassPath) {
+        File jarFile = new File(jar);
+        if (!jarFile.isAbsolute()) {
+          jarFile = new File(pluginDir + File.separatorChar + jar);
+        }
+
+        if (!classPath.contains(jarFile.getAbsoluteFile())) {
+          classPath.add(jarFile.getAbsolutePath());
+        }
+      }
+    }
+
+    List<String> typeGlobalClassPath = getSysProps().getStringList("jobtype.global.classpath", null, ",");
+    if (typeGlobalClassPath != null) {
+      for (String jar : typeGlobalClassPath) {
+        if (!classPath.contains(jar)) {
+          classPath.add(jar);
+        }
+      }
+    }
+
+    return classPath;
+  }
+
+  @Override
+  public void run() throws Exception {
+    File f = null;
+    if (shouldProxy && obtainTokens) {
+      userToProxy = getJobProps().getString("user.to.proxy");
+      getLog().info("Need to proxy. Getting tokens.");
+      Props props = new Props();
+      props.putAll(getJobProps());
+      props.putAll(getSysProps());
+
+      f = getHadoopTokens(props);
+      getJobProps().put("env." + HADOOP_TOKEN_FILE_LOCATION, f.getAbsolutePath());
+    }
+    try {
+      super.run();
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new Exception(e);
+    } finally {
+      if (f != null) {
+        try {
+          cancelHadoopTokens(f);
+        } catch (Throwable t) {
+          t.printStackTrace();
+          getLog().error("Failed to cancel tokens.");
+        }
+        if (f.exists()) {
+          f.delete();
+        }
+      }
+    }
+  }
+
+  private static String getSourcePathFromClass(Class<?> containedClass) {
+    File file = new File(containedClass.getProtectionDomain().getCodeSource().getLocation().getPath());
+
+    if (!file.isDirectory() && file.getName().endsWith(".class")) {
+      String name = containedClass.getName();
+      StringTokenizer tokenizer = new StringTokenizer(name, ".");
+      while (tokenizer.hasMoreTokens()) {
+        tokenizer.nextElement();
+
+        file = file.getParentFile();
+      }
+      return file.getPath();
+    } else {
+      return containedClass.getProtectionDomain().getCodeSource().getLocation().getPath();
+    }
+  }
+
+  protected File getHadoopTokens(Props props) throws HadoopSecurityManagerException {
+
+    File tokenFile = null;
+    try {
+      tokenFile = File.createTempFile("mr-azkaban", ".token");
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new HadoopSecurityManagerException("Failed to create the token file.", e);
+    }
+
+    hadoopSecurityManager.prefetchToken(tokenFile, props, getLog());
+
+    return tokenFile;
+  }
+
+  private void cancelHadoopTokens(File f) {
+    try {
+      hadoopSecurityManager.cancelTokens(f, userToProxy, getLog());
+    } catch (HadoopSecurityManagerException e) {
+      e.printStackTrace();
+      getLog().error(e.getCause() + e.getMessage());
+    } catch (Exception e) {
+      e.printStackTrace();
+      getLog().error(e.getCause() + e.getMessage());
+    }
+
+  }
+
+  @Override
+  protected String getJavaClass() {
+    return HadoopJavaJobRunnerMain.class.getName();
+  }
+
+  @Override
+  public String toString() {
+    return "JavaJob{" + "_runMethod='" + _runMethod + '\'' + ", _cancelMethod='" + _cancelMethod + '\''
+        + ", _progressMethod='" + _progressMethod + '\'' + ", _javaObject=" + _javaObject + ", props=" + getJobProps()
+        + '}';
+  }
 }
--- plugins/reportal/src/azkaban/jobtype/ReportalAbstractRunner.java
+++ plugins/reportal/src/azkaban/jobtype/ReportalAbstractRunner.java
@@ -43,130 +43,130 @@ import static org.apache.hadoop.security.UserGroupInformation.HADOOP_TOKEN_FILE_
 
 public abstract class ReportalAbstractRunner {
 
-	private static final String REPORTAL_VARIABLE_PREFIX = "reportal.variable.";
-	protected Props props;
-	protected OutputStream outputStream;
-	protected String proxyUser;
-	protected String jobQuery;
-	protected String jobTitle;
-	protected String reportalTitle;
-	protected String reportalStorageUser;
-	protected int outputCapacity;
-	protected Map<String, String> variables = new HashMap<String, String>();
+  private static final String REPORTAL_VARIABLE_PREFIX = "reportal.variable.";
+  protected Props props;
+  protected OutputStream outputStream;
+  protected String proxyUser;
+  protected String jobQuery;
+  protected String jobTitle;
+  protected String reportalTitle;
+  protected String reportalStorageUser;
+  protected int outputCapacity;
+  protected Map<String, String> variables = new HashMap<String, String>();
 
-	public ReportalAbstractRunner(Properties props) {
-		Props prop = new Props();
-		prop.put(props);
-		this.props = prop;
-	}
+  public ReportalAbstractRunner(Properties props) {
+    Props prop = new Props();
+    prop.put(props);
+    this.props = prop;
+  }
 
-	public void run() throws Exception {
-		System.out.println("Reportal: Setting up environment");
+  public void run() throws Exception {
+    System.out.println("Reportal: Setting up environment");
 
-		// Check the properties file
-		if (props == null) {
-			throw new ReportalRunnerException("Properties file not loaded correctly.");
-		}
+    // Check the properties file
+    if (props == null) {
+      throw new ReportalRunnerException("Properties file not loaded correctly.");
+    }
 
-		// Get the hadoop token
-		Configuration conf = new Configuration();
-		if (System.getenv(HADOOP_TOKEN_FILE_LOCATION) != null) {
-			conf.set(MAPREDUCE_JOB_CREDENTIALS_BINARY, System.getenv(HADOOP_TOKEN_FILE_LOCATION));
-		}
+    // Get the hadoop token
+    Configuration conf = new Configuration();
+    if (System.getenv(HADOOP_TOKEN_FILE_LOCATION) != null) {
+      conf.set(MAPREDUCE_JOB_CREDENTIALS_BINARY, System.getenv(HADOOP_TOKEN_FILE_LOCATION));
+    }
 
-		// Get properties
-		String execId = props.getString(CommonJobProperties.EXEC_ID);
-		outputCapacity = props.getInt("reportal.output.capacity", 10*1024*1024);
-		proxyUser = props.getString("reportal.proxy.user");
-		jobQuery = props.getString("reportal.job.query");
-		jobTitle = props.getString("reportal.job.title");
-		reportalTitle = props.getString("reportal.title");
-		reportalStorageUser = props.getString("reportal.storage.user", "reportal");
-		Map<String, String> reportalVariables = props.getMapByPrefix(REPORTAL_VARIABLE_PREFIX);
+    // Get properties
+    String execId = props.getString(CommonJobProperties.EXEC_ID);
+    outputCapacity = props.getInt("reportal.output.capacity", 10 * 1024 * 1024);
+    proxyUser = props.getString("reportal.proxy.user");
+    jobQuery = props.getString("reportal.job.query");
+    jobTitle = props.getString("reportal.job.title");
+    reportalTitle = props.getString("reportal.title");
+    reportalStorageUser = props.getString("reportal.storage.user", "reportal");
+    Map<String, String> reportalVariables = props.getMapByPrefix(REPORTAL_VARIABLE_PREFIX);
 
-		// Parse variables
-		for (Entry<String, String> entry: reportalVariables.entrySet()) {
-			if (entry.getKey().endsWith("from")) {
-				String fromValue = entry.getValue();
-				String toKey = entry.getKey().substring(0, entry.getKey().length() - 4) + "to";
-				String toValue = reportalVariables.get(toKey);
-				if (toValue != null) {
-					variables.put(fromValue, toValue);
-				}
-			}
-		}
+    // Parse variables
+    for (Entry<String, String> entry : reportalVariables.entrySet()) {
+      if (entry.getKey().endsWith("from")) {
+        String fromValue = entry.getValue();
+        String toKey = entry.getKey().substring(0, entry.getKey().length() - 4) + "to";
+        String toValue = reportalVariables.get(toKey);
+        if (toValue != null) {
+          variables.put(fromValue, toValue);
+        }
+      }
+    }
 
-		// Built-in variables
-		variables.put("run_id", execId);
-		variables.put("sys_date", Long.toString(System.currentTimeMillis() / 1000));
-		
-		Calendar cal = Calendar.getInstance();
-		Date date = new Date();
-		cal.setTime(date);
-		
-		String timeZone = props.getString("reportal.default.timezone", "UTC");
-		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
-		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		SimpleDateFormat hourFormat = new SimpleDateFormat("yyyy-MM-dd-HH");
-		
-		variables.put("hive_current_hour", hourFormat.format(cal.getTime()));
-		variables.put("hive_current_day", dateFormat.format(cal.getTime()));
-		cal.add(Calendar.HOUR, -1);
-		variables.put("hive_last_hour", hourFormat.format(cal.getTime()));
-		cal.add(Calendar.HOUR, 1);
-		cal.add(Calendar.DATE, -1);
-		variables.put("hive_yesterday", dateFormat.format(cal.getTime()));
-		cal.add(Calendar.DATE, -6);
-		variables.put("hive_last_seven_days", dateFormat.format(cal.getTime()));
-		cal.add(Calendar.DATE, -1);
-		variables.put("hive_last_eight_days", dateFormat.format(cal.getTime()));
-		variables.put("owner", proxyUser);
-		variables.put("title", reportalTitle);
+    // Built-in variables
+    variables.put("run_id", execId);
+    variables.put("sys_date", Long.toString(System.currentTimeMillis() / 1000));
 
-		// Props debug
-		System.out.println("Reportal Variables:");
-		for (Entry<String, String> data: variables.entrySet()) {
-			System.out.println(data.getKey() + " -> " + data.getValue());
-		}
+    Calendar cal = Calendar.getInstance();
+    Date date = new Date();
+    cal.setTime(date);
 
-		if (requiresOutput()) {
-			// Get output stream to data
-			String locationTemp = ("./reportal/" + jobTitle + ".csv").replace("//", "/");
-			File tempOutput = new File(locationTemp);
-			tempOutput.getParentFile().mkdirs();
-			tempOutput.createNewFile();
-			outputStream = new BoundedOutputStream(new BufferedOutputStream(new FileOutputStream(tempOutput)), outputCapacity);
+    String timeZone = props.getString("reportal.default.timezone", "UTC");
+    TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
 
-			// Run the reportal
-			runReportal();
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat hourFormat = new SimpleDateFormat("yyyy-MM-dd-HH");
 
-			// Cleanup the reportal
-			try {
-				outputStream.close();
-			} catch (IOException e) {
-				// We can safely ignore this exception since we're just making sure the stream is closed.
-			}
-		}
-		else {
-			runReportal();
-		}
-	}
+    variables.put("hive_current_hour", hourFormat.format(cal.getTime()));
+    variables.put("hive_current_day", dateFormat.format(cal.getTime()));
+    cal.add(Calendar.HOUR, -1);
+    variables.put("hive_last_hour", hourFormat.format(cal.getTime()));
+    cal.add(Calendar.HOUR, 1);
+    cal.add(Calendar.DATE, -1);
+    variables.put("hive_yesterday", dateFormat.format(cal.getTime()));
+    cal.add(Calendar.DATE, -6);
+    variables.put("hive_last_seven_days", dateFormat.format(cal.getTime()));
+    cal.add(Calendar.DATE, -1);
+    variables.put("hive_last_eight_days", dateFormat.format(cal.getTime()));
+    variables.put("owner", proxyUser);
+    variables.put("title", reportalTitle);
 
-	protected abstract void runReportal() throws Exception;
+    // Props debug
+    System.out.println("Reportal Variables:");
+    for (Entry<String, String> data : variables.entrySet()) {
+      System.out.println(data.getKey() + " -> " + data.getValue());
+    }
 
-	protected boolean requiresOutput() {
-		return true;
-	}
+    if (requiresOutput()) {
+      // Get output stream to data
+      String locationTemp = ("./reportal/" + jobTitle + ".csv").replace("//", "/");
+      File tempOutput = new File(locationTemp);
+      tempOutput.getParentFile().mkdirs();
+      tempOutput.createNewFile();
+      outputStream =
+          new BoundedOutputStream(new BufferedOutputStream(new FileOutputStream(tempOutput)), outputCapacity);
 
-	protected String injectVariables(String line) {
-		for (Entry<String, String> entry: variables.entrySet()) {
-			line = line.replace(":" + entry.getKey(), sanitizeVariable(entry.getValue()));
-		}
-		return line;
-	}
+      // Run the reportal
+      runReportal();
 
-	private String sanitizeVariable(String variable) {
-		return variable.replace("'", "\\'").replace("\"", "\\\"");
-	}
+      // Cleanup the reportal
+      try {
+        outputStream.close();
+      } catch (IOException e) {
+        // We can safely ignore this exception since we're just making sure the stream is closed.
+      }
+    } else {
+      runReportal();
+    }
+  }
+
+  protected abstract void runReportal() throws Exception;
+
+  protected boolean requiresOutput() {
+    return true;
+  }
+
+  protected String injectVariables(String line) {
+    for (Entry<String, String> entry : variables.entrySet()) {
+      line = line.replace(":" + entry.getKey(), sanitizeVariable(entry.getValue()));
+    }
+    return line;
+  }
+
+  private String sanitizeVariable(String variable) {
+    return variable.replace("'", "\\'").replace("\"", "\\\"");
+  }
 }
