9e6f7e07cf35c25e009b98197f120633e06985f3
Anthony Hsu <erwaman@gmail.com>
2015-03-22 20:45:14 -0700

    Remove extraneous comment

--- plugins/jobtype/src/azkaban/jobtype/hiveutils/azkaban/hive/actions/AlterTableLocationQL.java
+++ plugins/jobtype/src/azkaban/jobtype/hiveutils/azkaban/hive/actions/AlterTableLocationQL.java
@@ -17,8 +17,6 @@
 package azkaban.jobtype.hiveutils.azkaban.hive.actions;
 
 class AlterTableLocationQL implements HQL {
-  // alter table zoip set location
-  // 'hdfs://eat1-magicnn01.grid.linkedin.com:9000/user/jhoman/b';
   private final String table;
   private final String newLocation;
 
